b0a780c192f01a0fd20098595fe42ebc24d68427
Manya <manya@example.com>
2026-02-11 16:34:38 +0000

    smvitm shell script for directory creation

---
+++ exercise3/smvitm.sh
@@ -0,0 +1,15 @@
+mkdir cs ec ai ad me ce
+mkdir 6a 6b 6c
+cd 6a || exit
+mkdir bcs601 bcs602
+cd bcs601 || exit
+mkdir 6a 6b 6c
+touch 6a/cs_bcs601_6a_feedback
+touch 6b/cs_bcs601_6b_feedback
+touch 6c/cs_bcs601_6c_feedback
+cd ..
+cd bcs602 || exit
+mkdir 6a 6b 6c
+touch 6a/cs_bcs602_6a_feedback
+touch 6b/cs_bcs602_6b_feedback
+touch 6c/cs_bcs602_6c_feedback
